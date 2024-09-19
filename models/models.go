dee4a7525a5580e1ebc0f5fb990ce4d6b06c4caf
Mori <154768101+moriT958@users.noreply.github.com>
2024-09-19 19:06:42 +0900

    connect db

---
+++ models/models.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Todo struct {
+	TodoID      int       `json:"todo_id"`
+	Task        string    `json:"task"`
+	Done        bool      `json:"done"`
+	CreatedAt   time.Time `json:"created_at"`
+	CompletedAt time.Time `json:"completed_at"`
+}
