1b81d7413739488b74654022e74bbda5e3a42ec8
Mori <154768101+moriT958@users.noreply.github.com>
2024-09-29 13:15:50 +0900

    repositoryの単体テスト

--- repository/fixture/data.go
+++ repository/fixture/data.go
@@ -1,15 +1,15 @@
-package testdata
+package fixture
 
 import "go-todo/models"
 
 var TodoTestData = []models.Todo{
-	models.Todo{
+	{
 		Task: "test-todo1",
 	},
-	models.Todo{
+	{
 		Task: "test-todo2",
 	},
-	models.Todo{
+	{
 		Task: "test-todo3",
 	},
 }
