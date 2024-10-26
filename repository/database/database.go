6dae673624be3126edadbbaae40f3ab1b0e207ce
Mori <154768101+moriT958@users.noreply.github.com>
2024-10-26 18:31:54 +0900

    create move directory

--- repository/database/database.go
+++
@@ -1,9 +0,0 @@
-package database
-
-import "database/sql"
-
-type DB interface {
-	Query(query string, args ...any) (*sql.Rows, error)
-	QueryRow(query string, args ...any) *sql.Row
-	Exec(query string, args ...any) (sql.Result, error)
-}
